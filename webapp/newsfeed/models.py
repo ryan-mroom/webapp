9315a8365431f2d0ae88100bd18523781d3341ec
Ryan <ryan.mroom@gmail.com>
2019-11-12 16:09:20 +0200

    Created a database for the posts. Refactored newsfeed to query the Posts DB table.

--- webapp/newsfeed/models.py
+++ webapp/newsfeed/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    source = models.CharField(max_length=64)
+    link_url = models.CharField(max_length=128)
+    link_title = models.CharField(max_length=128)
+    tags = models.CharField(max_length=128)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
