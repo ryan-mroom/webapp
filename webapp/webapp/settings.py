58c0e45041b238433ebb61eae61a93c82383b56e
Ryan <ryan.mroom@gmail.com>
2019-11-12 17:03:24 +0200

    Completed backend mechanisms to validate and save users to the users db

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'newsfeed.apps.NewsfeedConfig',
     'users.apps.UsersConfig',
+
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
