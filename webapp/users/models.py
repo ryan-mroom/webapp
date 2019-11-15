c738ce0025418e88e872b2851d770b04a0482379
Ryan <ryan.mroom@gmail.com>
2019-11-15 12:16:07 +0200

    Added profile and user info update functionality.

--- webapp/users/models.py
+++ webapp/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+    # NOTE: Need to delete old image before adding a new one.
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 256 or img.width > 256:
+            output_size = (256, 256)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
