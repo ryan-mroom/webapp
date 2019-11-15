c664d02a35d6ba343e411d408c9754d66bfd6bd2
Ryan <ryan.mroom@gmail.com>
2019-11-15 18:09:33 +0200

    Refactored Newsfeed functional view into a class based view. Added functionality to create posts, edit posts, delete posts with the appropriate security conditions.

--- webapp/newsfeed/urls.py
+++ webapp/newsfeed/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='newsfeed-home'),
+    path('', PostListView.as_view(), name='newsfeed-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='newsfeed-post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='newsfeed-post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='newsfeed-post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='newsfeed-post-delete'),
 ]
--- webapp/newsfeed/views.py
+++ webapp/newsfeed/views.py
@@ -1,19 +1,61 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from django.forms.models import model_to_dict
 
 
-def home(request):
-    posts = [model_to_dict(Post.objects.all()[0])]
-    for post in posts:
-        post.update({'tags_array': get_tags(post)})
-
-    context = {
-        'title': 'Newsfeed',
-        'posts': posts
-    }
-    return render(request, 'newsfeed/home.html', context)
+def get_tags(post_tags):
+    return post_tags.split(" ")
 
 
-def get_tags(post):
-    return post['tags'].split(" ")
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        for post in data['object_list']:
+            post.tags = get_tags(post.tags)
+        return data
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['post'].tags = get_tags(data['post'].tags)
+        return data
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'source', 'link_url', 'link_title', 'tags']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'source', 'link_url', 'link_title', 'tags']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = 'users-dashboard'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
