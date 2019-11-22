0cae727ffe2d299e3ad57ce83710a51cf568422e
Ryan <ryan.mroom@gmail.com>
2019-11-22 10:14:58 +0200

    Updated default image for posts.

--- webapp/newsfeed/models.py
+++ webapp/newsfeed/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     tags = models.CharField(max_length=128)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='newsfeed-default.jpg', upload_to='newsfeed_pics/')
+    image = models.ImageField(default='newsfeed-default.png', upload_to='newsfeed_pics/')
 
     def __str__(self):
         return self.title
